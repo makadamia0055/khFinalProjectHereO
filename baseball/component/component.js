b444277045d1060cd36be66a7146c9ab96540e20
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-02-25 19:33:43 +0900

    update color

--- baseball/component/component.js
+++ baseball/component/component.js
@@ -3,17 +3,21 @@ $(function () {
   $(".submenu__background").hide();
 
   $(".top-menu__team-hover").hover(function(){
+    $(".submenu__background").css({"background-color" : "#FFC54D"});
     $(".submenu__background").show();
     $(".team-submenu__01").show();
   },function(){
+    $(".submenu__background").css({"background-color" : "#fffbe9"});
     $(".submenu-containerBox1").hide();
     $(".submenu__background").hide();
   })
 
   $(".top-menu__commu-hover").hover(function(){
+    $(".submenu__background").css({"background-color" : "#FFC54D"});
     $(".submenu__background").show();
     $(".commu-submenu__01").show();
   },function(){
+    $(".submenu__background").css({"background-color" : "#fffbe9"});
     $(".submenu-containerBox1").hide();
     $(".submenu__background").hide();
   })
