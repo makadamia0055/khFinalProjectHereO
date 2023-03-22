6db6a25cd0aafe7ab7608315be58e5d551ef514a
makadamia0055 <makend91@naver.com>
2023-03-22 19:39:55 +0900

    마이 바티스 의존성 추가, 폴더 구조, 기타 리소스 파일 추가(js, css파일들)

--- hereoTest/src/main/java/com/hereo/project/controller/HomeController.java
+++ hereoTest/src/main/java/com/hereo/project/controller/HomeController.java
@@ -1,12 +1,10 @@
-package com.hereo.test;
+package com.hereo.project.controller;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HomeController {
@@ -18,3 +16,4 @@ public class HomeController {
 	}
 	
 }
+	
