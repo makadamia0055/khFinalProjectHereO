bea29cf9c9b24bae82a28e7835544915475fcbdb
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-04-13 18:03:54 +0900

    메시지유틸추가

--- hereoTest/src/main/java/com/hereo/project/controller/CommuController.java
+++ hereoTest/src/main/java/com/hereo/project/controller/CommuController.java
@@ -3,12 +3,16 @@ package com.hereo.project.controller;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hereo.project.service.CommuService;
+import com.hereo.project.utils.MessageUtils;
 import com.hereo.project.vo.BoardTypeVO;
 import com.hereo.project.vo.MembersVO;
 
@@ -41,13 +45,20 @@ public class CommuController {
 	public String market() {
 		return "/community/market-board";
 	}
-	
+
 	@GetMapping(value = "/community/writing")
-	public String writing(HttpServletRequest request) {
+	public String writing(
+			HttpServletRequest request, 
+			HttpServletResponse response,
+			Model model
+			) {
+		String referer = request.getHeader("Referer");
 		MembersVO user=(MembersVO)request.getSession().getAttribute("loginUser");
-		ArrayList<BoardTypeVO> btList = boardService.getBoardType(user.getMe_siteauth());
-		
+		if (user==null) {
+			MessageUtils.alertAndGoPrepage(response, "로그인 기능이 필요한 서비스입니다.", referer);
+		}
 		return "/community/commu-writingboard";
+		
 	}
 }
 	
