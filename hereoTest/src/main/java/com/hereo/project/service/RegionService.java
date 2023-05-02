326d1239e97deb640a7d4719073b05350f96b615
kwon younghyeon <kyh1834@gmail.com>
2023-05-02 16:25:49 +0900

    지역등록(수정예정)

---
+++ hereoTest/src/main/java/com/hereo/project/service/RegionService.java
@@ -0,0 +1,27 @@
+package com.hereo.project.service;
+
+import java.util.List;
+
+import com.hereo.project.vo.RegionDetailVO;
+import com.hereo.project.vo.RegionSubVO;
+import com.hereo.project.vo.RegionVO;
+
+public interface RegionService {
+
+	List<RegionVO> getRegion();
+
+	RegionSubVO getRegionSub(RegionSubVO regionSub);
+
+	void insertRegionDetail(RegionDetailVO regionDetail);
+
+	void insertRegion(RegionVO region);
+
+	List<RegionSubVO> getRegionSubList(int re_num);
+
+	void insertRegionSub(RegionSubVO regionSub);
+
+	List<RegionDetailVO> getRegionDetail(int rd_num);
+
+
+	
+}
