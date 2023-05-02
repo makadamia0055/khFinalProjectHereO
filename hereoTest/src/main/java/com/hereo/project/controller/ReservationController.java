7cf862b2c815f5f92f90546c0a76c3da5c0a030d
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-05-02 21:38:19 +0900

    update

--- hereoTest/src/main/java/com/hereo/project/controller/ReservationController.java
+++ hereoTest/src/main/java/com/hereo/project/controller/ReservationController.java
@@ -1,22 +1,41 @@
 package com.hereo.project.controller;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hereo.project.dao.RegionDAO;
 import com.hereo.project.service.ReservationService;
+import com.hereo.project.utils.MessageUtils;
+import com.hereo.project.vo.BoardCategoryVO;
+import com.hereo.project.vo.BoardTypeVO;
+import com.hereo.project.vo.MembersVO;
+import com.hereo.project.vo.RegionVO;
 
 
 @Controller
 public class ReservationController {
 	@Autowired
 	ReservationService reservationService;
-
+	
+	@Autowired
+	RegionDAO regionDao;
 	//예약 메인 페이지
 	@RequestMapping(value = "/reservation/main", method = RequestMethod.GET)
 	public ModelAndView reservationMainPage(ModelAndView mv) {
+		RegionVO[]regionList = regionDao.selectAllRegion();
+		
+		mv.addObject("regionList", regionList);
 		mv.setViewName("/reservation/reservation-main");
 		return mv;
 	}
